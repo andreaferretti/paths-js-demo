63cfff528fa247a4801b7d684e20528a7f5e3c6b
Andrea Ferretti <ferrettiandrea@gmail.com>
2013-12-14 01:03:03 +0100

    First commit

---
+++ js/main.js
@@ -0,0 +1,1 @@
+(function(){(function(){requirejs.config({baseUrl:"./js",paths:{text:"../components/requirejs-text/text",json:"../components/requirejs-plugins/src/json",ractive:"../components/ractive/build/Ractive",paths:"../components/paths-js/dist/paths",templates:"../templates",data:"../data"}}),require(["app"],function(){})}).call(this),define("main",function(){})})();
